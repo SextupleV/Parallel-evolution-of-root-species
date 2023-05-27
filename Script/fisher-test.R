e04515427243c9255e53084c70920dba894140ab
Wenwu Wu <wwwu@zafu.edu.cn>
2023-05-27 20:12:51 +0800

    Update fisher-test.R

--- Script/fisher-test.R
+++ Script/fisher-test.R
@@ -13,15 +13,15 @@ data <- read.table("inputfile",header = T,row.names = 1)
 
 row_number <- nrow(data )
 for (x in 1:row_number) {
-  test =data [x,1:4]
+  test <- data [x,1:4]
   test <- as.numeric(test)
   test <- matrix(test,nrow=2)
   out <- fisher.test(test,alternative = "greater")  # the alternative must be one of "two.sided", "greater" or "less".
   data[x,5] <- out$p.value
 }
 names(data)[5]<-"pvalue"
-data$fdr  = p.adjust(data$pvalue,method = "BH")  # p.adjust.methods :holm, hochberg, hommel, bonferroni, BH, BY, fdr, none
-data$negative_log10fdr = -log10(data$fdr)
+data$fdr  <- p.adjust(data$pvalue,method = "BH")  # p.adjust.methods :holm, hochberg, hommel, bonferroni, BH, BY, fdr, none
+data$negative_log10fdr <- -log10(data$fdr)
 
 write.table (data,"fisher_result.tab", row.names = T,col.names =T, quote =F,sep="\t")
 
