d19ab2fb6452d0afdf3a2a661cf74304bada6f71
Wenwu Wu <wwwu@zafu.edu.cn>
2023-05-29 10:56:23 +0800

    Add files via upload

---
+++ Script/Correlation.R
@@ -0,0 +1,43 @@
+#!/usr/bin/Rscript
+
+
+##################
+
+# Evaluate the correlation between plant height and three gene derived modes.
+
+
+data = read.table("tableS5",header = T)
+
+data$Hight=log2(data$Hight+1)
+
+#### HGT
+
+plot(data$hight,data$Number_of_HGT-derived_genes,pch=16,cex=2,xlim = c(0,7),ylim = c(0.05,200))
+
+abline(lm(data$Number_of_HGT-derived_genes~data$Hight))
+
+cor.test(data$Hight,data$Number_of_HGT-derived_genes)
+
+
+
+#### WGD
+
+plot(data$hight,data$Number_of_WGD-derived_genes,pch=16,cex=2,xlim = c(0,7),ylim = c(0.05,40000))
+
+abline(lm(data$Number_of_WGD-derived_genes~data$Hight))
+
+cor.test(data$Hight,data$Number_of_WGD-derived_genes)
+
+
+#### TD
+
+plot(data$hight,data$Number_of_TD-derived_genes,pch=16,cex=2,xlim = c(0,7),ylim = c(0.05,20000))
+
+abline(lm(data$Number_of_TD-derived_genes~data$Hight))
+
+cor.test(data$Hight,data$Number_of_TD-derived_genes)
+
+
+
+
+
