a28d0ea4738203f1dfa1f56d6e931d913b91e7fa
Wenwu Wu <wwwu@zafu.edu.cn>
2023-05-29 11:12:53 +0800

    Add files via upload

---
+++ Script/WoodyANDHerbs.R
@@ -0,0 +1,41 @@
+#!/usr/bin/Rscript
+
+
+##################
+
+# Evaluate the difference in three gene modes between woody and herbaceous plants.
+
+
+library(ggplot2)
+
+data = read.table("tableS4",header = T)
+
+# HGT
+
+ggplot(data,aes(x = Habit, y = Number_of-HGT-derived_genes)) +
+  geom_boxplot(fill = c('green','blue'), show.legend = FALSE, width = 0.7) + 
+  geom_jitter(width = 0.2, alpha = 0.6) 
+
+wilcox.test(data$Number_of-HGT-derived_genes[data$Habit=='Herbaceous'],data$Number_of-HGT-derived_genes[data$Habit=='Woody'],alternative = 'two.side',exact=T)
+
+
+
+# WGD
+
+ggplot(data,aes(x = Habit, y = Number_of-WGD-derived_genes)) +
+  geom_boxplot(fill = c('green','blue'), show.legend = FALSE, width = 0.7) + 
+  geom_jitter(width = 0.2, alpha = 0.6) 
+
+wilcox.test(data$Number_of-WGD-derived_genes[data$Habit=='Herbaceous'],data$Number_of-WGD-derived_genes[data$Habit=='Woody'],alternative = 'two.side',exact=T)
+
+
+# TD
+
+ggplot(data,aes(x = Habit, y = Number_of-TD-derived_genes)) +
+  geom_boxplot(fill = c('green','blue'), show.legend = FALSE, width = 0.7) + 
+  geom_jitter(width = 0.2, alpha = 0.6) 
+
+wilcox.test(data$Number_of-TD-derived_genes[data$Habit=='Herbaceous'],data$Number_of-TD-derived_genes[data$Habit=='Woody'],alternative = 'two.side',exact=T)
+
+
+
