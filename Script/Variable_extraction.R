6eb725dfe379a00aacfeacf10a65e680b8ff21c2
Wenwu Wu <wwwu@zafu.edu.cn>
2023-09-11 16:04:33 +0800

    Create Variable_extraction.R

---
+++ Script/Variable_extraction.R
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+
+setwd("/working/gridded_variables/")
+
+var_list <- c(list.files(pattern = "\\.tif$", full.names = TRUE))
+
+for (x in 1:length(var_list)){
+  var = var_list[x]
+  
+  gbif=read.csv(geo_coordinates,header = T,sep="\t")
+  gbif_out=gbif
+  gbif=data.frame(gbif$decimalLongitude,gbif$decimalLatitude)
+  dfsp = SpatialPoints(gbif)
+  geovar=raster(var)
+  gbif_out$var_value <- raster::extract(geovar,dfsp)
+  
+  write.table(gbif_out,paste(var,"_variables",sep=""), row.names = F, col.names =T, quote =T,sep="\t")
+  
+}
