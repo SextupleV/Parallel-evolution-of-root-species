c396ec4356a0bc78e35a301855c9d9d2922f33de
Wenwu Wu <wwwu@zafu.edu.cn>
2023-09-11 16:36:03 +0800

    Create Fisher-test.R

---
+++ Code/Script/Fisher-test.R
@@ -0,0 +1,26 @@
+#!/usr/bin/Rscript
+
+# Inputfile is in the following format (tab separated):
+
+# group	num1	num2	num3	num4
+# a	100	200	500	3000
+# b	200	300	600	4000
+# c	300	400	700	5000
+#        ... 
+
+
+data <- read.table("inputfile",header = T,row.names = 1)
+
+row_number <- nrow(data )
+for (x in 1:row_number) {
+  test <- data [x,1:4]
+  test <- as.numeric(test)
+  test <- matrix(test,nrow=2)
+  out <- fisher.test(test,alternative = "greater")
+  data[x,5] <- out$p.value
+}
+names(data)[5]<-"pvalue"
+data$fdr <- p.adjust(data$pvalue,method = "BH")
+data$negative_log10fdr <- -log10(data$fdr)
+
+write.table (data,"fisher_result.tab", row.names = T,col.names =T, quote =F,sep="\t")
