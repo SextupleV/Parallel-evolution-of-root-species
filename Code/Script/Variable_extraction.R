848e80426ca5301737358bc8fbed5eb88b9ce2fe
Wenwu Wu <wwwu@zafu.edu.cn>
2023-09-12 08:39:59 +0800

    Update Variable_extraction.R

--- Code/Script/Variable_extraction.R
+++ Code/Script/Variable_extraction.R
@@ -10,7 +10,7 @@ var_list <- c(list.files(pattern = "\\.tif$", full.names = TRUE))
 for (x in 1:length(var_list)){
   var <- var_list[x]
   
-  gbif <- read.csv(geo_coordinates.txt,header = T,sep="\t") # geo_coordinates.txt file can be obtained from the 'inputfile_for_script' folder
+  gbif <- read.csv(geo_coordinates.txt,header = T,sep="\t") # 'geo_coordinates.txt' file can be obtained from the 'inputfile_for_script' folder
   gbif_out <-  gbif
   gbif <- data.frame(gbif$decimalLongitude,gbif$decimalLatitude)
   dfsp <- SpatialPoints(gbif)
